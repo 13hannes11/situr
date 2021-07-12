4ec8b766cd23ceff7dc4304e13c99c914776e4be
Hannes F. Kuchelmeister <hannes@kuchelmeister.org>
2021-07-12 15:59:32 +0200

    add package info and licencse

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='situr',
+      version='0.1',
+      description='A package to register situ images',
+      url='https://github.com/13hannes11/situr',
+      author='Hannes F. Kuchelmeister',
+      author_email='hannes@kuchelmeister.org',
+      license='MIT',
+      packages=['situr'],
+      zip_safe=False)
