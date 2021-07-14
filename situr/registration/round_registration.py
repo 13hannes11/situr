f1e801d07ede1ac2f5740643d9b6ee8b05060529
Hannes F. Kuchelmeister <hannes@kuchelmeister.org>
2021-07-14 11:03:30 +0200

    add todo to round registration

--- situr/registration/round_registration.py
+++ situr/registration/round_registration.py
@@ -14,6 +14,8 @@ class RoundRegistration(Registration):
             reference_round (int, optional): The round that is referenced and will not be changed. Defaults to 0.
             reference_channel (int, optional): The channel tha is used to compare rounds. Defaults to 0.
         """
+
+        # TODO: instead of one reference channel use all channels (maybe without nucleus channel)
         reference_peaks = situ_tile.get_image_round(
             reference_round).get_channel_peaks(reference_channel)
         for round in range(situ_tile.get_roundcount()):
